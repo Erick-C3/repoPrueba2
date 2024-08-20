2c89b4f488a964df598160da8d25434ab89f29e8
Erick <erick.nahuelpaiva.001@gmail.com>
2024-08-20 17:18:59 -0300

    mejora calc

--- app.js
+++ app.js
@@ -3,10 +3,32 @@ console.log(document.querySelector("input").value+" un mensaje");
  */
 const inputOperaciones = document.querySelector("input");
 
+
+const OPCION_RESULTADO = "=";
+const OPCION_LIMPIAR = "C";
+const OPCION_BORRAR_ULTIMO = "←";
+const PANTALLA_VACIA = "";
+const OPERADORES = "+-*/";
+const NUMEROS = "0123456789";
+
 function actualizarVentana(boton) {
-    if (boton.innerHTML === "=") {
-        inputOperaciones.value = eval(inputOperaciones.value);
+    let ultimoCaracter = inputOperaciones.value[inputOperaciones.value.length -1];
+    if (!OPERADORES.includes(ultimoCaracter) || NUMEROS.includes(boton.innerHTML)) {
+        switch (boton.innerHTML) {
+            case OPCION_RESULTADO:
+                inputOperaciones.value = eval(inputOperaciones.value);
+                break;
+            case OPCION_LIMPIAR:
+                inputOperaciones.value = PANTALLA_VACIA;
+                break;
+            case OPCION_BORRAR_ULTIMO:
+                inputOperaciones.value = inputOperaciones.value.slice(0,-1);
+                break;
+            default:
+                inputOperaciones.value += boton.innerHTML;
+                break;
+        }
     }else{
-        inputOperaciones.value += boton.innerHTML;
+        alert("opcion no permitida")
     }
 }
